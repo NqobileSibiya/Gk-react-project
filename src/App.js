aa28d485ab78b1029925477d3ac0f87291602037
Nqobile Sibiya <nqobileamandlasibiya@gmail.com>
2024-06-29 23:43:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React,{useState} from "react";
+import Navbar from "./components/Navbar/Navbar";    
+import Sidebar from "./components/Sidebar/Sidebar";                  
+import Form from "./components/Form/Form";
+import Notes from "./components/Notes/Notes";
+import Modal from "./components/Modal/Modal";
+
+const NOTES = [
+    // {
+    //   id: "a1234",
+    //   title: "some title1",
+    //   text: "some text1"
+    // },
+  ]
+const App = () => {
+  const [notes, setNotes] = useState(NOTES);
+  const [selectedNote, setSelectedNote] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  const addNote = (note) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  };
+
+  const editNote = (editedNote) => {
+    setNotes((prevNotes) => {
+      const newArray = prevNotes.map((note) => {
+        if (editedNote.id === note.id) {
+          note.title = editedNote.title;
+          note.text = editedNote.text;
+        }
+        return note;
+      })
+      return newArray;
+    });
+  }
+
+  const deleteNote = (id) =>{
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note) => id !== note.id);
+    });
+  };
+  const toggleModal = () => {
+    setSelectedNote(notes);
+    setIsModalOpen(prevState => {
+    return !prevState;
+   })
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+      <Form addNote={addNote} />
+      <Notes
+        notes={notes}
+        deleteNote={deleteNote}
+        toggleModal={toggleModal}
+        setSelectedNote={setSelectedNote}
+      />
+      {isModalOpen && (
+        <Modal
+          isModalOpen={isModalOpen}
+          selectedNote={selectedNote}
+          toggleModal={toggleModal}
+          editNote={editNote}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
+
+
